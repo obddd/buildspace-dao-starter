d000c9882f8604a4731363343030a29afc0d3f2e
obddd <obaidulhaq20@gmail.com>
2021-12-21 23:20:12 +0500

    connected wallet using 3rd web

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address);
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to XDao</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          🥳 Connect your wallet!
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
